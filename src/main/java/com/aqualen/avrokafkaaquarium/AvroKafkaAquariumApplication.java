8fb9325e52acbbf743b216a28aa9adcd9c342013
elenbeilina <elenbeilina>
2021-02-25 00:10:12 +0300

    Initial commit

---
+++ src/main/java/com/aqualen/avrokafkaaquarium/AvroKafkaAquariumApplication.java
@@ -0,0 +1,13 @@
+package com.aqualen.avrokafkaaquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroKafkaAquariumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvroKafkaAquariumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aqualen/avrokafkaaquarium/AvroKafkaAquariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqualen.avrokafkaaquarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroKafkaAquariumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
