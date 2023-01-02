81002dbac795624974571eac045b28f93a151a40
beiliele <Elena.Beilina@externals.adidas.com>
2023-01-02 15:06:34 +0530

    - added Spring metrics - added monitoring

--- data/add-shark.sh
+++ data/add-shark.sh
@@ -2,4 +2,4 @@ printf " -> Adding shark to aquarium \n\n"
 
 curl -X PUT \
  -H "Content-Type: application/json"  \
- http://localhost:8888/aquarium/send-shark
+ http://localhost:9000/aquarium/send-shark
