d44ae31fa5893a13de2e1b1e6c542c65533f245d
elenbeilina <Elena.Beilina@externals.adidas.com>
2023-05-27 13:52:51 +0700

    - added security to the actuator endpoints - migrated to 3 spring boot

--- src/main/java/com/aqualen/avrokafkaaquarium/conf/AvroSerde.java
+++ src/main/java/com/aqualen/avrokafkaaquarium/conf/AvroSerde.java
@@ -2,20 +2,21 @@ package com.aqualen.avrokafkaaquarium.conf;
 
 import com.aqualen.Shark;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import io.confluent.kafka.serializers.subject.TopicRecordNameStrategy;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.apache.kafka.common.serialization.Serde;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
 @Component
+@NoArgsConstructor
 public class AvroSerde {
 
     @Value("${registry.url}")
@@ -31,10 +32,12 @@ public class AvroSerde {
     private Map<String, Object> getConfig() {
         final Map<String, Object> serdeConfig = new HashMap<>();
 
-        serdeConfig.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, registry);
+        serdeConfig.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, registry);
         serdeConfig.put(AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS, true);
-        serdeConfig.put(AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY, TopicRecordNameStrategy.class.getName());
-        serdeConfig.put(AbstractKafkaAvroSerDeConfig.KEY_SUBJECT_NAME_STRATEGY, TopicRecordNameStrategy.class.getName());
+        serdeConfig.put(AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY,
+                TopicRecordNameStrategy.class.getName());
+        serdeConfig.put(AbstractKafkaAvroSerDeConfig.KEY_SUBJECT_NAME_STRATEGY,
+                TopicRecordNameStrategy.class.getName());
 
         return serdeConfig;
     }
---
+++ src/main/java/com/aqualen/avrokafkaaquarium/conf/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.aqualen.avrokafkaaquarium.conf;
+
+import lombok.SneakyThrows;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http.authorizeHttpRequests()
+                .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+                .requestMatchers("/aquarium/**").permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN");
+        http.csrf().and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(bCryptPasswordEncoder.encode("pass"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("admin")
+                .password(bCryptPasswordEncoder.encode("pass"))
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    @SneakyThrows
+    AuthenticationManager authenticationManager(HttpSecurity http,
+                                                BCryptPasswordEncoder bCryptPasswordEncoder,
+                                                UserDetailsService userDetailService) {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
